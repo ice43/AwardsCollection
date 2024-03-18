6a6cb5fcb6b33470295ab1269819a5d5a8d440e3
bowski43 <43mngt@gmail.com>
2024-03-18 10:32:30 +0300

    Initial Commit

---
+++ AwardsCollection/AwardsCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionApp.swift
+//  AwardsCollection
+//
+//  Created by Serge Broski on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
