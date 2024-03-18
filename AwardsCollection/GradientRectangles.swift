01b01e3f4935d9841156e180267d03b2958d9e8a
bowski43 <43mngt@gmail.com>
2024-03-18 14:02:01 +0300

    some fixes

--- AwardsCollection/GradientRectangles.swift
+++ AwardsCollection/GradientRectangles.swift
@@ -9,30 +9,25 @@ import SwiftUI
 
 struct GradientRectangles: View {
     var body: some View {
-        GeometryReader { geometry in
-            let width = geometry.size.width
-            let height = geometry.size.height
-            
-            ZStack {
-                ForEach(0..<3) { iteration in
-                    Rectangle()
-                        .fill(
-                            LinearGradient(
-                                gradient: Gradient(colors: [.green, .blue]),
-                                startPoint: .bottomLeading,
-                                endPoint: .topTrailing
-                            )
+        ZStack {
+            ForEach(0..<3) { iteration in
+                Rectangle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [.green, .blue]),
+                            startPoint: .bottomLeading,
+                            endPoint: .topTrailing
                         )
-                        .rotationEffect(.degrees(Double(iteration) * 60))
-                        .scaleEffect(0.7)
-                }
-                
-                Image(systemName: "airplane")
-                    .resizable()
-                    .rotationEffect(.degrees(-90))
-                    .opacity(0.5)
+                    )
+                    .rotationEffect(.degrees(Double(iteration) * 60))
                     .scaleEffect(0.7)
             }
+            
+            Image(systemName: "airplane")
+                .resizable()
+                .rotationEffect(.degrees(-90))
+                .opacity(0.5)
+                .scaleEffect(0.7)
         }
     }
 }
